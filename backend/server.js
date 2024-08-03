a62fc6092bc80020b44209524a3d9fb4e4dad413
Girish More <girishmore097@gmail.com>
2024-08-03 20:03:20 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,12 @@ const db = require('./config/db');
 const cors = require('cors');
 
 const app = express();  
-app.use(cors());  
-
+const corsOptions = {
+  origin: 'https://timetablehub.netlify.app/', // replace with your Netlify site URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
  
 db.connect();
